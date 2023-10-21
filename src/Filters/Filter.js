5c7a274b08cb477fcd44494b05e645fafee656d9
Nicole <15011784+nicolelaine@users.noreply.github.com>
2023-10-21 15:20:49 +0200

    props.toggleFilter has been added, with an argument that indicates WHICH input has been clicked

--- src/Filters/Filter.js
+++ src/Filters/Filter.js
@@ -1,17 +1,30 @@
 import React from "react";
+import { hotels } from "../data.js";
 
-/* functional component
-    - accepts the data model for a single filter as a prop
-    - uses that data to render the UI for a single filter in the list
-*/
-const Filter = (props) => (
-  <li className="filter">
-    <span>
-      <input type="checkbox" value={true} className="filter__checkbox" />
-      {props.filter.name}
-    </span>
-    <span className="filter__match-count">{props.filter.matchCount}</span>
-  </li>
-);
+const getMatchCount = (filterKey) =>
+  hotels.filter((hotel) => hotel[filterKey]).length;
+
+const Filter = (props) => {
+  /* it is not enough to just call props.toggleFilter when the input 
+  is clicked, we need to call props.toggleFilter with an argument 
+  that indicates WHICH input has been clicked, so we create this
+  onChange function here for each Filter component that is rendered */
+  const onChange = () => props.toggleFilter(props.filter.key);
+  const matchCount = getMatchCount(props.filter.key);
+  return (
+    <li className="filter">
+      <span>
+        <input
+          type="checkbox"
+          checked={props.isChecked}
+          onChange={onChange}
+          className="filter__checkbox"
+        />
+        {props.filter.display}
+      </span>
+      <span className="filter__match-count">{matchCount}</span>
+    </li>
+  );
+};
 
 export default Filter;
