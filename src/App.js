fe6f6b957c18cbc05fc337129163ab025c8c32fd
Nicole <15011784+nicolelaine@users.noreply.github.com>
2023-10-21 23:40:17 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from "react";
-import HotelsList from "./Hotels/index";
-import Filters from "./Filters/index";
-import { filterData, hotelData } from "./data.js";
+import React, { Component } from "react";
+import Hotels from "./Hotels";
+import Filters from "./Filters";
 
-const App = () => (
-  <div className="App">
-    <Filters filters={filterData} />
-    <HotelsList hotels={hotelData} />
-  </div>
-);
+class App extends Component {
+  state = { selectedFilters: [] };
+  toggleFilter = (clickedFilterKey) => {
+    let newFilters;
+    // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes to see how the .includes array method works
+    const alreadySelected = this.state.selectedFilters.includes(
+      clickedFilterKey
+    );
+    if (alreadySelected) {
+      // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter to see how the .filter array method works
+      newFilters = this.state.selectedFilters.filter(
+        (selectedFilter) => selectedFilter !== clickedFilterKey
+      );
+    } else {
+      // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat to see how the .concat array method works
+      newFilters = this.state.selectedFilters.concat(clickedFilterKey);
+    }
+    this.setState({ selectedFilters: newFilters });
+  };
+  render() {
+    return (
+      <div className="App">
+        <Filters
+          selectedFilters={this.state.selectedFilters}
+          toggleFilter={this.toggleFilter}
+        />
+        <Hotels selectedFilters={this.state.selectedFilters} />
+      </div>
+    );
+  }
+}
 
 export default App;
