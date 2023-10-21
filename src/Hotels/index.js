ecfcc3425b9e46d11b6af98b0fe830b57bae47d3
Nicole <15011784+nicolelaine@users.noreply.github.com>
2023-10-21 15:19:24 +0200

    The callback function has been moved inside of the HotelsList function so that you can access props in your definition of the renderHotel callback function.

--- src/Hotels/index.js
+++ src/Hotels/index.js
@@ -1,15 +1,21 @@
-import React from "react";
+mport React from "react";
 import Hotel from "./Hotel.js";
+import { hotels } from "../data.js";
 
 const renderHotel = (hotel) => <Hotel key={hotel.id} hotel={hotel} />;
 
 const HotelsList = (props) => {
-  if (!props.hotels) {
-    return null;
-  }
+  /* see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter to learn how the .filter array method works
 
-  const hotelListElements = props.hotels.map((hotel) => renderHotel(hotel));
+  see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every to learn how the .every array method works
+  */
+  const filterCallback = (hotel) => {
+    const everyCallback = (filter) => hotel[filter];
+    return props.selectedFilters.every(everyCallback);
+  };
+  const filteredHotels = hotels.filter(filterCallback);
 
+  const hotelListElements = filteredHotels.map(renderHotel);
   return <ul className="hotels-list">{hotelListElements}</ul>;
 };
 
